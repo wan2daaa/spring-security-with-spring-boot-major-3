bbadbd120f334950a268604c66a6a41829c84a13
wan2daaa <wan2daaa@gmail.com>
2023-04-24 17:35:35 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/wane/study/springsecuritywithbootver3/SpringSecurityWithBootVer3Application.java
@@ -0,0 +1,13 @@
+package wane.study.springsecuritywithbootver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithBootVer3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithBootVer3Application.class, args);
+    }
+
+}
---
+++ src/test/java/wane/study/springsecuritywithbootver3/SpringSecurityWithBootVer3ApplicationTests.java
@@ -0,0 +1,13 @@
+package wane.study.springsecuritywithbootver3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithBootVer3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
